6a22d452ccce94b9715d12cb2816bb223aa0f1ad
RamonWill <ramonwilliams@hotmail.co.uk>
2020-06-24 19:21:54 +0100

    setup for admin functions

---
+++ DataProjectCsharp/Services/AdminService.cs
@@ -0,0 +1,28 @@
+﻿using DataProjectCsharp.Data;
+using DataProjectCsharp.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProjectCsharp.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly IAdminRepository _adminRepo;
+        private readonly AlphaVantageConnection _avConn;
+        public AdminService(IAdminRepository adminRepo, AlphaVantageConnection avConn)
+        {
+            this._adminRepo = adminRepo;
+            this._avConn = avConn;
+        }
+        public void UpdateSecurityPrices(string symbol)
+        {
+            // so basically use the GetOpenTradeTickers() to get a list of the open tickers.
+            // then for each ticker GetMostRecentPrice(symbol) which returns a date datetime.
+            // then connect get the prices from the alphavantage connection and add prices for that ticker if the date is more recent than the most recent date.
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataProjectCsharp/Services/IAdminService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProjectCsharp.Services
+{
+    interface IAdminService
+    {
+        void UpdateSecurityPrices(string symbol);
+    }
+}
